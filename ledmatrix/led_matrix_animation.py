5616b3011eb69df3c7fde5cf26297604efa6dab5
VeetiMcinen <veeti.makinen141@gmail.com>
2024-04-18 10:57:48 +0300

    piece of shit usb stick almost fucked me over

--- ledmatrix/led_matrix_animation.py
+++ ledmatrix/led_matrix_animation.py
@@ -1,7 +1,6 @@
 from rpi_ws281x import Color #type: ignore
 import time
 import random
-from Drawings import * #Maybe remove?
 from strip_definition import strip, led_matrix
 
 # LED module configuration:
@@ -16,9 +15,8 @@ def clearstrip():
         strip.setPixelColor(counter, Color(0, 0, 0))
     strip.show()
 
-def draw(coordinates, strip=strip, set_color=False, shuffle_animation=False, reverse_animation=False, wait_time=0):
+def draw(character, strip=strip, set_color=False, shuffle_animation=False, reverse_animation=False, wait_time=0):
     clearstrip()  # Ensure the strip is cleared before setting new pixels
-
     colors = set_color if set_color else randomcolor()
 
     if reverse_animation and shuffle_animation:
@@ -26,12 +24,12 @@ def draw(coordinates, strip=strip, set_color=False, shuffle_animation=False, rev
         return
 
     if reverse_animation:
-        coordinates.reverse()
+        character.reverse()
     
     if shuffle_animation:
-        random.shuffle(coordinates)
+        random.shuffle(character)
     
-    for row, col in coordinates:
+    for row, col in character:
         led_index = row * 8 + col
         strip.setPixelColor(led_index, Color(colors[0], colors[1], colors[2]))
         if wait_time != 0:
@@ -39,5 +37,10 @@ def draw(coordinates, strip=strip, set_color=False, shuffle_animation=False, rev
             time.sleep(wait_time)
     if wait_time == 0:
         strip.show()
+
+def graph_function(equation, strip=strip):
+    default_coordinates = [(0,7),(1,7),(2,7),(3,7),(4,7),(5,7),
+                           (6,7),(7,7),(7,0),(7,1),(7,2),(7,3),
+                           (7,4),(7,5),(7,6),(7,7)]
 
 strip.begin()
--- ledmatrix/main.py
+++ ledmatrix/main.py
@@ -1,7 +1,6 @@
 from led_matrix_animation import draw
 from Drawings import *
-import time
 
 if __name__ == '__main__':
     #coordinates, set_color=False, shuffle_animation=False, reverse_animation=False
-    draw(letterA, set_color=(255,0,0), wait_time = 0.03)
+    draw(letter_s, wait_time = 0.03)
