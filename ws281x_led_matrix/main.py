a28d0aac06e62534c7b9764f69e5c6b49c1133f1
VeetiMcinen <veeti.makinen141@gmail.com>
2024-04-18 15:08:52 +0300

    what the fuck

--- ws281x_led_matrix/main.py
+++
@@ -1,6 +0,0 @@
-from led_matrix_animation import draw
-from Drawings import *
-
-if __name__ == '__main__':
-    #coordinates, set_color=False, shuffle_animation=False, reverse_animation=False
-    draw(letter_s, wait_time = 0.03)
