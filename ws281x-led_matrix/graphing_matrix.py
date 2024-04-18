1d76fe101d6032e0929e5e96412204da46cdd57e
VeetiMcinen <veeti.makinen141@gmail.com>
2024-04-18 15:48:52 +0300

    progress finally

--- ws281x-led_matrix/graphing_matrix.py
+++ ws281x-led_matrix/graphing_matrix.py
@@ -1,4 +1,4 @@
-from rpi_ws281x import PixelStrip, Color # type: ignore
+#from rpi_ws281x import PixelStrip, Color
 LED_COUNT = 49  # Total number of LEDs in a 7x7 matrix
 LED_PIN = 12  # GPIO pin connected to the pixels (must support PWM)
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800kHz)
@@ -9,7 +9,11 @@ LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53, otherwise '0'
 # Define borders based on a 0-indexed system for a 7x7 matrix
 RIGHT_BORDER = [6, 13, 20, 27, 34, 41, 48]  # Last column of each row in 7x7
 LEFT_BORDER = [0, 7, 14, 21, 28, 35, 42]  # First column of each row in 7x7
-graphing_strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+#graphing_strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+
+
+#TODO LIST: Graph function, highlight x and y coordinates
+
 matrix_size = 7
 # Create a list to represent the matrix, each entry starts with False
 led_matrix = [False] * (matrix_size * matrix_size)
@@ -24,17 +28,21 @@ for y in range(3, -4, -1):  # y starts at 3 and decreases to -3
         coordinates.append([x, y, index, False])  # Include the boolean value as the fourth element
 
 # Optional: Print to check the mapping and initial states
-for item in coordinates:
-    print(f"Coordinate ({item[0]}, {item[1]}) maps to index {item[2]} in the LED matrix, initial state: {item[3]}.")
 
 # Example of how to update a specific LED state
 # If you want to turn on the LED at coordinate (0, 0) which corresponds to the center of the matrix
-center_index = (3 - 0) * matrix_size + (0 + 3)
+x_coordinate = 0
+y_coordinate = 0
+center_index = (3 - x_coordinate) * matrix_size + (y_coordinate + 3)
 led_matrix[center_index] = True  # Set the LED state to True in the 1D representation
 
 # Update the corresponding entry in the 'coordinates' list
+print(center_index)
 for coord in coordinates:
     if coord[2] == center_index:
         coord[3] = True  # Update the state in the structured list
 
-print(f"Updated state of index {center_index}: {led_matrix[center_index]}")
+
+for coordinate in coordinates:
+    if coordinate[3]:
+        print(f"({coordinate[0]}, {coordinate[1]})")
